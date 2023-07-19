4c814e6f46cdc1cf39f9084d405959299533969d
iLGZH-y <dearmengqiyuan@163.com>
2023-07-20 00:42:41 +0800

    Update coc setting

--- after/plugin/coc.lua
+++ after/plugin/coc.lua
@@ -6,8 +6,14 @@ vim.opt.signcolumn = "yes"
 
 local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
 
+function _G.check_back_space()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
 -- use <TAB>
-vim.keymap.set("i", "<TAB>", "coc#pum#visible() ? coc#pum#next(1) : CheckBackspace() ? '<TAB>' : coc#refresh()", opts)
+vim.keymap.set("i", "<TAB>", "coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? '<TAB>' : coc#refresh()",
+    opts)
 
 -- use <S-TAB>
 vim.keymap.set("i", "<S-TAB>", "coc#pum#visible() ? coc#pum#prev(1) : '<C-h>'", opts)
@@ -26,3 +32,28 @@ vim.keymap.set("n", "gd", "<Plug>(coc-definition)", { silent = true })
 vim.keymap.set("n", "gy", "<Plug>(coc-type-definition)", { silent = true })
 vim.keymap.set("n", "gi", "<Plug>(coc-implementation)", { silent = true })
 vim.keymap.set("n", "gr", "<Plug>(coc-references)", { silent = true })
+
+function _G.show_docs()
+    local cw = vim.fn.expand('<cword>')
+    if vim.fn.index({ 'vim', 'help' }, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h' .. cw)
+    elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+    else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+    end
+end
+
+vim.keymap.set("n", "<leader>m", "<CMD>lua _G.show_docs()<CR>", { silent = true })
+
+local _opts = { silent = true, nowait = true, expr = true }
+vim.keymap.set("n", "<C-f>", 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"', _opts)
+vim.keymap.set("n", "<C-b>", 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"', _opts)
+
+vim.keymap.set("i", "<C-f>",
+    'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(1)<cr>" : "<Right>"', _opts)
+vim.keymap.set("i", "<C-b>",
+    'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(0)<cr>" : "<Left>"', _opts)
+
+vim.keymap.set("v", "<C-f>", 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"', _opts)
+vim.keymap.set("v", "<C-b>", 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"', _opts)
