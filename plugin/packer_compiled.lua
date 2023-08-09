92b31a4d9174daa5acf71f45abcc4de72236aa73
iLGZH <dearmengqiyuan@163.com>
2023-08-09 13:31:14 +0800

    add copilot adn wait to complete

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -89,6 +89,11 @@ _G.packer_plugins = {
     path = "/home/ymq/.local/share/nvim/site/pack/packer/start/coc.nvim",
     url = "https://github.com/neoclide/coc.nvim"
   },
+  ["copilot.vim"] = {
+    loaded = true,
+    path = "/home/ymq/.local/share/nvim/site/pack/packer/start/copilot.vim",
+    url = "https://github.com/github/copilot.vim"
+  },
   fzf = {
     loaded = true,
     path = "/home/ymq/.local/share/nvim/site/pack/packer/start/fzf",
