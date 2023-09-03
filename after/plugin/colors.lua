75c8730457296be834ec4f65ef43caa8725edcd2
iLGZH <dearmengqiyuan@163.com>
2023-09-03 14:42:34 +0800

    Update color

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,6 +3,7 @@ function ColorMyPencils(color)
 
     if (color == 'tokyonight-night') then
         require("tokyonight").setup({
+            style = "night",
             transparent = true,
             styles = {
                 sidebars = "transparent",
@@ -10,11 +11,11 @@ function ColorMyPencils(color)
             },
         })
     end
-    vim.cmd [[ set background=dark ]]
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- vim.cmd [[ set background=dark ]]
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils('tokyonight-night')
